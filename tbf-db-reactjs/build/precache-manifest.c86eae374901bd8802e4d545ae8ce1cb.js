4519d7b704d58b94a7e251afa335be22a20f516f
Eric Pallad <e.pallad@pp-itservices.de>
2020-08-01 08:21:00 +0200

    first build

---
+++ tbf-db-reactjs/build/precache-manifest.c86eae374901bd8802e4d545ae8ce1cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ad1e7203cdc613d573d9c9f21cd7d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b367960fd83a1cc76369",
+    "url": "/static/css/main.db941864.chunk.css"
+  },
+  {
+    "revision": "6f218be7e65b8b4bb302",
+    "url": "/static/js/2.2011e1ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2011e1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b367960fd83a1cc76369",
+    "url": "/static/js/main.1253e913.chunk.js"
+  },
+  {
+    "revision": "0112f553f83bb31af25f",
+    "url": "/static/js/runtime-main.68974b47.js"
+  }
+]);
