73bf0bdf73f1833793b095649ca88f7a08f12a1d
Eric Pallad <e.pallad@pp-itservices.de>
2020-10-27 15:46:01 +0100

    enhanced status reporting

--- tbf-db-backend/importTable.php
+++ tbf-db-backend/importTable.php
@@ -39,55 +39,54 @@
         return $fileName;
     };
 
+    if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+        $targetFilePath = convertToUTF8($sourceFile);
+        $fields = $con->query($fieldquery);
+        if($fields) {
+            $firstRowNames = "";
+            $firstRow = preg_replace('/\s+/', ' ', trim(fgets(fopen($targetFilePath, "r"))));
+            $firstRow = explode(",",$firstRow);
+            foreach($firstRow as $var) {
+                $firstRowNames .= "`".$var."`,";
+            }
+            $firstRowNames = substr($firstRowNames, 0, -1);
+
+            $con->query("CREATE TEMPORARY TABLE temporary_table SELECT * FROM `$table` WHERE 1=0");
+            if($con->error !== "") {
+                $statusMsg[] = $con->error;
+            }
+            $con->query("LOAD DATA LOCAL INFILE '$targetFilePath' INTO TABLE temporary_table FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES ($firstRowNames)");
+            if($con->error !== "") {
+                $statusMsg[] = $con->error;
+            }
+            $tableKey = mysqli_fetch_assoc(mysqli_query($con, "SHOW KEYS FROM `$table` WHERE Key_name = 'PRIMARY'"));
+            $con->query("INSERT INTO `$table` SELECT * FROM temporary_table ON DUPLICATE KEY UPDATE `".$tableKey["Column_name"]."` = VALUES(".$tableKey["Column_name"].")");
+            $statusMsg[] = $con->info;
+            $statusMsg[] = $con->affected_rows . " Zeilen importiert";
+            $con->query("DROP TEMPORARY TABLE temporary_table;");
+
+            //$query = "LOAD DATA LOCAL INFILE '$targetFilePath' INTO TABLE `$table` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES ($firstRowNames)";
+            /*
+            if($fieldCompare == $firstRow) {
+                $query = "LOAD DATA LOCAL INFILE '$targetFilePath' INTO TABLE `$table` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES ($fieldNames)";
+            } else {
+                $query = "LOAD DATA LOCAL INFILE '$targetFilePath' INTO TABLE `$table` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ($fieldNames)";
+            }
+            */
+            //$insert = $con->query($query);
+            //if($insert) {
+        } else {
+            $statusMsg[] = $con->error;
+            $statusMsg[] = "Leider konnte die Datenbank nicht gelesen werden";
+        }
+    } else {
+        $statusMsg[] = "Leider konnte die Datei nicht hochgeladen werden";
+    }
+
     switch ($method) {
         case 'POST':
             header('Content-Type: application/json');
             header('Access-Control-Allow-Origin: *');
-            if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
-                $targetFilePath = convertToUTF8($sourceFile);
-                $fields = $con->query($fieldquery);
-                if($fields) {
-                    $firstRowNames = "";
-                    $firstRow = preg_replace('/\s+/', ' ', trim(fgets(fopen($targetFilePath, "r"))));
-                    $firstRow = explode(",",$firstRow);
-                    foreach($firstRow as $var) {
-                        $firstRowNames .= "`".$var."`,";
-                    }
-                    $firstRowNames = substr($firstRowNames, 0, -1);
-
-                    $con->query("CREATE TEMPORARY TABLE temporary_table SELECT * FROM `$table` WHERE 1=0");
-                    $con->query("LOAD DATA LOCAL INFILE '$targetFilePath' INTO TABLE temporary_table FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES ($firstRowNames)");
-                    $tableKey = mysqli_fetch_assoc(mysqli_query($con, "SHOW KEYS FROM `$table` WHERE Key_name = 'PRIMARY'"));
-                    $con->query("INSERT INTO `$table` SELECT * FROM temporary_table ON DUPLICATE KEY UPDATE `".$tableKey["Column_name"]."` = VALUES(".$tableKey["Column_name"].")");
-                    $statusMsg[] = $con->info;
-                    $con->query("DROP TEMPORARY TABLE temporary_table;");
-
-                    //$query = "LOAD DATA LOCAL INFILE '$targetFilePath' INTO TABLE `$table` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES ($firstRowNames)";
-                    /*
-                    if($fieldCompare == $firstRow) {
-                        $query = "LOAD DATA LOCAL INFILE '$targetFilePath' INTO TABLE `$table` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES ($fieldNames)";
-                    } else {
-                        $query = "LOAD DATA LOCAL INFILE '$targetFilePath' INTO TABLE `$table` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ($fieldNames)";
-                    }
-                    */
-                    //$insert = $con->query($query);
-                    //if($insert) {
-                    if($con->error == "") {
-                        if($con->affected_rows > 0) {
-                            $statusMsg[] = "Erfolgreich $con->affected_rows Zeilen importiert";
-                        } else {
-                            $statusMsg[] = "Upload erfolgreich. Keine neue Zeilen importiert";
-                        } 
-                    } else {
-                        $statusMsg[] = "Fehlgeschlagen: " . $con->error;
-                    }
-                } else {
-                    $statusMsg[] = $con->error;
-                    $statusMsg[] = "Leider konnte die Datenbank nicht gelesen werden";
-                }
-            } else {
-                $statusMsg[] = "Leider konnte die Datei nicht hochgeladen werden";
-            }
             echo json_encode($statusMsg);
             break;
         default:
