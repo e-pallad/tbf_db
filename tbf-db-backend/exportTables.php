5ae3a13aa975f7bd07c6249a3fbaba3d4f5401d5
Eric <60659440+e-pallad@users.noreply.github.com>
2020-10-29 10:39:30 +0100

    added footer and include new rotation script

--- tbf-db-backend/exportTables.php
+++ tbf-db-backend/exportTables.php
@@ -16,7 +16,7 @@
                 `AKZ_Gr2_Anlagenteil`,
                 `AKZ_Gr3_Aggregat`,
                 `AKZ_Gr4_Nummer`,
-                `AKZ_Gr5_Nummer`,
+                `AKZ_Gr5_Aggregat`,
                 `AKZ_Gr6_Nummer`) 
             AS `AKZ Kodierung` 
         FROM 
@@ -24,50 +24,6 @@
         LEFT JOIN 
             `RI-TBF_SEF_Elektroangaben` 
         ON 
-        ";
-    } elseif ($table == "SEF_Messstellenliste") {
-        $query = "
-        SELECT 
-        CONCAT_WS(
-            '.',
-            `AKZ_Gr1_Standort`,
-            `AKZ_Gr2_Anlagenteil`,
-            `AKZ_Gr3_Aggregat`,
-            `AKZ_Gr4_Nummer`,
-            `AKZ_Gr5_Aggregat`,
-            `AKZ_Gr6_Nummer`) 
-        AS `AKZ Kodierung`, 
-            `Funktion_Stoff`,
-            `Funktion_Cod.`,
-        CONCAT(
-            `Funktion_Signal_High`,
-            ', ',
-            `Funktion_Signal_Low`)
-        AS `Funktion_Signal`,
-            `Schaltanlage`,
-            `Messbereich`,
-            `Ausgangssignal`,
-            `Spannungsversorgung`,
-            `Messverfahren`,
-            `Anzahl der Grenzkontakte`,
-            `Selbstüberwachung + Störmeldekontakt`,
-            `Sicherungsautomat`,
-            `NH-Trenner`,
-            `Überspannungsschutz`,
-            `FI-Schutzschalter`,
-            `Wartungsschalter`,
-            `Vor-Ort-Anzeige`,
-            `Anzeige Schaltschrank`,
-            `Anzeige Bedientafel`,
-            `Anzeige im PLS`,
-            `Erneuern VO`,
-            `Erneuern EMSR`,
-            `Schutzart`,
-            `Ex-Schutz`,
-            `zu Bearbeiten`,
-            `Zusatzgeräte/Bemerkungen`
-        FROM 
-            `RI-TBF_SEF_Messstellenliste`
         ";
     } else {
         $query = "SELECT * FROM `$table`";
